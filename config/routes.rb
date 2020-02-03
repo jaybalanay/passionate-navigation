c85d467b2a923184a2354b7b319791e5a2706525
Jay Balanay <manolet.balanay@gmail.com>
2020-02-04 06:29:08 +0800

    Remove unnecessary logs, Add routes for verticals/categories/courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #
+  namespace :api do
+    namespace :v1, defaults: { format: :json } do
+      resources :verticals do
+        resources :categories do
+          resources :courses
+        end
+      end
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,11 +16,7 @@ verticals_json_file = File.read(Rails.root.join('db', 'json', 'verticals.json'))
 JSON.parse(verticals_json_file).each do |vertical|
   vertical_params = vertical.transform_keys!(&:downcase).symbolize_keys
 
-  puts ">>>>> Creating vertical with params: #{vertical_params} <<<<<"
-
   Vertical.create(vertical_params)
-
-  puts ">>>>> DONE <<<<<"
 end
 
 categories_json_file = File.read(Rails.root.join('db', 'json', 'categories.json'))
@@ -29,13 +25,9 @@ JSON.parse(categories_json_file).each do |category|
   vertical        = Vertical.find_by(id: category_hash[:verticals])
   category_params = category_hash.except(:verticals)
 
-  puts ">>>>> Creating category with params: #{category_params} <<<<<"
-
   category          = Category.new(category_params)
   category.vertical = vertical
   category.save
-
-  puts ">>>>> DONE <<<<<"
 end
 
 courses_json_file = File.read(Rails.root.join('db', 'json', 'courses.json'))
@@ -44,12 +36,8 @@ JSON.parse(courses_json_file).each do |course|
   category      = Category.find_by(id: course_hash[:categories])
   course_params = course_hash.except(:category)
 
-  puts ">>>>> Creating course with params: #{course_params} <<<<<"
-
   course          = Course.new(course_params)
   course.category = category
   course.save
-
-  puts ">>>>> DONE <<<<<"
 end
 
